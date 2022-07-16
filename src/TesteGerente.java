f5df749e34858b6f64bc0f7a39d98b3e63a8d81c
jvramos-py <jvramos411@gmail.com>
2022-07-16 19:19:02 -0300

    Usando herança.

---
+++ src/TesteGerente.java
@@ -0,0 +1,15 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Eduardo");
+        g1.setCpf("qualquer um");
+        g1.setSalario(5000);
+        g1.setSenha(2222);
+
+        System.out.println(g1.getNome());
+
+
+        boolean autenticou = g1.autentica(2222);
+        System.out.println(autenticou);
+    }
+}
